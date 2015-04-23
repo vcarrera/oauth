85d248dfe92cabf3230e8c126b28f76926ac0ac5
Victor Carrera arrieta <vcarrera@alumnos.urjc.es>
2015-04-23 14:46:10 +0200

    prueba1

---
+++ oauth.js
@@ -0,0 +1,14 @@
+var OAUTH_PROXY_URL = {
+'local.knarly.com' : 'http://local.knarly.com:5500/proxy'
+}[window.location.hostname] || 'https://auth-server.herokuapp.com/proxy';
+
+hello.init({ 	
+	github: a741c13af9e8cb773389
+}, { redirect_uri: 'https://vcarrera.github.io/oauth/rediret.html',
+     oauth_proxy : OAUTH_PROXY_URL  });
+
+$(document).ready(hello( 'github' ).login().then( function(){
+	alert('You are signed in to github');
+}, function( e ){
+	alert('Signin error: ' + e.error.message);
+}));
